d16ef25551bf3f03b41959ac56d6dd97fe8540a5
Thomas Graeff <676@holbertonschool.com>
2020-02-25 08:05:18 +0000

    Recursive solution

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,12 +6,8 @@ Find the minimum amount of operations using only Copy All and Paste to reach n
 
 def minOperations(n):
     '''find minimum amount'''
-    steps = n
-    factor = 0
     if n <= 1:
         return 0
-    if ((n % 2 != 0) and (n % 3 != 0)):
-        return n
     else:
         for i in range(2, n + 1):
             if n % i == 0:
