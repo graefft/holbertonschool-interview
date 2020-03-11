2d1776f57e9e59f4c2de9e0cce1b7506468c82c8
Thomas Graeff <676@holbertonschool.com>
2020-03-11 07:24:48 +0000

    Trying to find requirement error for checker

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -5,8 +5,8 @@
 
 int add_sandpiles(int grid1[3][3], int grid2[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void print_grid(int grid[3][3]);
 void print_grid_sum(int grid1[3][3], int grid2[3][3]);
 void topple_sandpile(int grid1[3][3], int grid2[3][3]);
+void print_grid(int grid[3][3]);
 
-#endif /*__SANDPILES_H__*/
+#endif
