afcef7d08e3eea7cb93c1686c350cbefa82cc2ae
Thomas Graeff <676@holbertonschool.com>
2020-04-06 17:53:55 +0000

    Adds print at EOF

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -34,6 +34,7 @@ if __name__ == '__main__':
                 pass
             if count % 10 == 0:
                 print_stats()
+        print_stats()
     except KeyboardInterrupt:
         print_stats()
         raise
