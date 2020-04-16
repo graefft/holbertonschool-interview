025f8ea053cc322ca885b9da0d7a0c6aacdc5dc2
Thomas Graeff <676@holbertonschool.com>
2020-04-16 07:07:40 +0000

    Break into two functions

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -11,60 +11,86 @@
  */
 int slide_line(int *line, size_t size, int direction)
 {
-	int lo = 0, hi = 0, value = 0;
-
 	if (!line | !size)
 		return (0);
 
 	if (direction == SLIDE_LEFT)
+		slide_left(line, size);
+	else if (direction == SLIDE_RIGHT)
+		slide_right(line, size);
+	return (1);
+}
+
+/**
+ * slide_left - slides array to the left
+ *
+ * @line: array of integers
+ * @size: size of array
+ * Return: void
+ */
+void slide_left(int *line, size_t size)
+{
+	int lo = 0;
+	int hi = 0;
+	int value = 0;
+
+	for (hi = 1; hi < (int)size; hi++)
 	{
-		for (hi = 1; hi < (int)size; hi++)
+		if (line[hi])
 		{
-			if (line[hi])
+			if (!value)
+				value = line[hi];
+			if (value == line[hi])
 			{
-				if (!value)
-					value = line[hi];
-				if (value == line[hi])
-				{
-					line[lo] = value * 2;
-					printf("LO = [%d]\n", line[lo]);
-					value = 0;
-				}
-				else
-				{
-					line[lo] = value;
-					value = line[hi];
-				}
-			lo++;
-			}
-		}
-		if (value)
-			line[lo] = value;
-		while (lo < (int)size)
-		{
-			line[lo] = 0;
-			lo++;
-		}
-	}
-	if (direction == SLIDE_RIGHT)
-	{
-		hi = size - 1;
-		value = line[hi];
-		for (lo = size - 2; lo >= 0; lo--)
-		{
-			if (value == line[lo])
-			{
-				line[hi] = value * 2;
+				line[lo] = value * 2;
+				printf("LO = [%d]\n", line[lo]);
 				value = 0;
 			}
 			else
 			{
-				line[hi] = value;
-				value = line[lo];
+				line[lo] = value;
+				value = line[hi];
 			}
+		lo++;
 		}
-		if (value)
-			line[hi] = value;
 	}
-	return (1);
+	if (value)
+		line[lo] = value;
+	while (lo < (int)size)
+	{
+		line[lo] = 0;
+		lo++;
+	}
+}
+
+/**
+ * slide_right - slides the array to the right
+ *
+ * @line: array of integers
+ * @size: size of array
+ * Return: void
+ */
+void slide_right(int *line, size_t size)
+{
+	int lo = 0;
+	int hi = 0;
+	int value = 0;
+
+	hi = size - 1;
+	value = line[hi];
+	for (lo = size - 2; lo >= 0; lo--)
+	{
+		if (value == line[lo])
+		{
+			line[hi] = value * 2;
+			value = 0;
+		}
+		else
+		{
+			line[hi] = value;
+			value = line[lo];
+		}
+	}
+	if (value)
+		line[hi] = value;
 }
