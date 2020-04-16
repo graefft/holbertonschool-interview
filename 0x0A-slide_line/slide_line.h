14ce6602b7136d8a8ab82d412f1471d1e7334510
Thomas Graeff <676@holbertonschool.com>
2020-04-16 06:56:59 +0000

    Adds header file

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef __SLIDE_LINE__
+#define __SLIDE_LINE__
+
+#define SLIDE_LEFT -1
+#define SLIDE_RIGHT 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /* __SLIDE_LINE__ */
