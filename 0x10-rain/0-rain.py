7919836b967c54bbc32f463f0dd038f159ca8466
Thomas Graeff <676@holbertonschool.com>
2020-06-10 19:16:49 +0000

    Refactor

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -9,25 +9,21 @@ def rain(walls):
     '''calculates how much water will be retained after it rains'''
     if len(walls) == 0:
         return 0
-    u = 0
-    w = 0
-    lo = 0
-    hi = 0
-    for i in range(len(walls)):
-        w = 0
-        while ((walls[lo] < 1) and lo < len(walls)):
-            lo = lo + 1
-        if hi < len(walls) - 1:
-            hi = lo + 1
-        while ((walls[hi] < 1) and hi < len(walls)):
-            hi = hi + 1
-            w = w + 1
-        if walls[hi] > walls[lo]:
-            u = u + (w * walls[lo])
-        else:
-            u = u + (w * walls[hi])
-        print('u = {} | w = {} | lo = {} | hi = {}'.format(u, w, lo, hi))
-        lo = lo + 1
-        if hi == len(walls) - 1:
-            break
-    return u
+
+    result = 0
+
+    for i in range(1, len(walls) - 1):
+        # Find maximum element on left
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+
+        # Find maximum element on right
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+
+        # Update maximum water
+        result = result + (min(left, right) - walls[i])
+
+    return result
