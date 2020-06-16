f74d7df7ebab78e6b7ca56792bd80a04ecb5fac7
Thomas Graeff <676@holbertonschool.com>
2020-06-16 07:00:09 +0000

    Adds heap sort

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * heap_sort - sorts an array of integers in ascending order - sift-down
+ * @array: array to sort
+ * @size: size of array
+ */
+void heap_sort(int *array, size_t size)
+{
+	
