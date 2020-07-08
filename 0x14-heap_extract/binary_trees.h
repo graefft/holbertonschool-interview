3ed39e8de2eb7e19cc205bd9345146b70cc6bc35
Thomas Graeff <676@holbertonschool.com>
2020-07-08 17:12:03 +0000

    Initial add of heap_extract

--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef __BINARY_TREES_H__
 #define __BINARY_TREES_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -21,5 +24,9 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
 
 int heap_extract(heap_t **root);
+size_t binary_tree_height(const binary_tree_t *tree);
+int is_perfect_recursion(const binary_tree_t *tree, int depth, int level);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+void full_heapify(heap_t *node);
 
 #endif
