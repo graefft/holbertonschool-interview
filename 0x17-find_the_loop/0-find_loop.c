1534cc27f8b838a59eef65b295afa4b072bb24ae
Thomas Graeff <676@holbertonschool.com>
2020-07-28 00:59:08 -0700

    Initial commit of function to find loop in singly-linked list

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds a loop in a singly-linked list
+ *
+ * @head: head of list
+ *
+ * Return: node where cycle starts, or NULL if there is no cycle
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = NULL;
+	listint_t *fast = NULL;
+
+	if (!head)
+		return (NULL);
+
+	slow = head;
+	fast = head;
+
+	while (fast->next->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			break;
+	}
+	while (fast)
+	{
+		for (slow = head; slow != fast; slow = slow->next)
+		{
+			if (slow == fast->next)
+				return (slow);
+		}
+		fast = fast->next;
+	}
+	return (NULL);
+}
