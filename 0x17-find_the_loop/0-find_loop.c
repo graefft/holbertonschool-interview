6b24b11526c05be530d9a5c8b7ea017435804a3b
Thomas Graeff <676@holbertonschool.com>
2020-07-28 01:10:56 -0700

    Refactors code

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -12,27 +12,26 @@ listint_t *find_listint_loop(listint_t *head)
 	listint_t *slow = NULL;
 	listint_t *fast = NULL;
 
-	if (!head || !(head->next))
+	if (!head)
 		return (NULL);
 
 	slow = head;
 	fast = head;
 
-	while (fast->next->next)
+	while (fast->next->next && fast)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 		if (slow == fast)
-			break;
-	}
-	while (fast)
-	{
-		for (slow = head; slow != fast; slow = slow->next)
 		{
-			if (slow == fast->next)
-				return (slow);
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
 		}
-		fast = fast->next;
 	}
 	return (NULL);
 }
