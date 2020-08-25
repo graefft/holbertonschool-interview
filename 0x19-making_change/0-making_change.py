73f4cbc20018b060b4845080a21f28f11ddf174b
Thomas Graeff <676@holbertonschool.com>
2020-08-25 01:12:24 -0700

    Initial commit of coin counting function

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Given a pile of coins of different values, determine the fewest number of coins
+needed to meet a given amount total.
+'''
+
+
+def makeChange(coins, total):
+    '''return fewest number of coins needed'''
+    if total <= 0:
+        return -1
+    coins.sort(reverse=True)
+    i = 0
+    count = 0
+    for i in range(len(coins)):
+        while ((total - coins[i]) >= 0):
+            total = total - coins[i]
+            count += 1
+    if (total == 0):
+        return count
+    else:
+        return -1
