922e2e32a6ffa541b18b1e195cdd07a52a56fff4
Thomas Graeff <676@holbertonschool.com>
2020-08-27 23:24:24 -0700

    Adds header file

---
+++ 0x1A-double_circular_linked_list/list.h
@@ -0,0 +1,25 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct List - doubly linked list
+ * @str: string - (malloc'ed string)
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct List
+{
+	char *str;
+	struct List *prev;
+	struct List *next;
+} List;
+
+List *add_node_end(List **list, char *str);
+
+#endif /* __LIST_H__ */
