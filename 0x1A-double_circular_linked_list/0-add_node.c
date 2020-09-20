bade3b10abf36e47cf3877c080b2298fe6be36d1
Thomas Graeff <676@holbertonschool.com>
2020-09-19 18:50:22 -0700

    Checks for strdup failure

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -21,6 +21,12 @@ List *add_node_begin(List **list, char *str)
 		return (NULL);
 	}
 	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node->str);
+		perror("strdup");
+		return (NULL);
+	}
 
 	if (*list)
 	{
