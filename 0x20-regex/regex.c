299fb786e4635e1d894b2dc76ca435ef6a914cd0
Thomas Graeff <676@holbertonschool.com>
2020-10-14 23:59:10 -0700

    Initial commit

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,16 @@
+#include "regex.h"
+
+#define MAX_ERROR_MSG 0x1000
+
+/**
+ * regex_match - checks whether a given pattern matches a given string
+ *
+ * @str: string to scan
+ * @pattern: regular expression
+ * Return: 1 if pattern matches, otherwise 0
+ */
+int regex_match(char const *str, char const *pattern)
+{
+
+	return (1);
+}
