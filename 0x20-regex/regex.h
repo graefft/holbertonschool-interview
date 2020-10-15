434052512ef18f599ebcb6164b5ecee4838f5bf2
Thomas Graeff <676@holbertonschool.com>
2020-10-14 23:58:18 -0700

    Adds header file

---
+++ 0x20-regex/regex.h
@@ -0,0 +1,11 @@
+#ifndef __REGEX_H__
+#define __REGEX_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <regex.h>
+
+int regex_match(char const *str, char const *pattern);
+
+#endif /* __REGEX_H __ */
