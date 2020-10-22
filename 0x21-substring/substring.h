2853835e6fc32fffb4201b4c9855d1b9b8d2c885
Thomas Graeff <676@holbertonschool.com>
2020-10-22 00:01:14 -0700

    Betty formatting

--- 0x21-substring/substring.h
+++ 0x21-substring/substring.h
@@ -10,22 +10,22 @@
 #define SEGMENT_COUNT 256
 
 /**
- * trie_node
+ * struct Trie - trie node
  *
- * @tc: child nodes
+ * @children: child nodes
  * @total: total number of instances of a word
  * @count: decremented during searching
  * @isWord: boolean 1 if end of word, 0 if not
  * @next: linked list node
  */
-struct Trie
+typedef struct Trie
 {
 	struct Trie *children[TRIE_SIZE];
 	int total;
 	int count;
 	int isWord;
 	struct tnode *next;
-};
+} Trie;
 
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
 
