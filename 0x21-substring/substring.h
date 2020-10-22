89c3f8477c4511eb45e803f5077ac18b6e01bb8d
Thomas Graeff <676@holbertonschool.com>
2020-10-21 22:31:15 -0700

    Adds header file

---
+++ 0x21-substring/substring.h
@@ -0,0 +1,10 @@
+#ifndef __SUBSTRING_H__
+#define __SUBSTRING_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif /* __SUBSTRING_H__ */
